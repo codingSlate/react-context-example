91de9c0bc8758e80867efa3e594863bbc8469572
codingSlate <ananta2021k@gmail.com>
2022-02-13 14:59:41 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 import './style.css';
+import Home from './Home';
 import Login from './Login';
 import Products from './Products';
 import UserProfile from './UserProfile';
@@ -9,23 +10,23 @@ import ProtectedRoute from './ProtectedRoute';
 import AppState from './context/AppState';
 import AppContext from './context/AppContext';
 
-
 export default function App() {
   // const { isAuth } = useContext(AppContext);
   return (
     <div>
       <BrowserRouter>
-        <Link to="/">Login</Link>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
         <Link to="/products">Products</Link>
         <Link to="/profile">Profile</Link>
         <AppState>
           <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/products" element={<Products />} />
-            <Route element={<ProtectedRoute/>}>
+            <Route element={<ProtectedRoute />}>
               <Route path="/profile" element={<UserProfile />} />
             </Route>
-
           </Routes>
         </AppState>
       </BrowserRouter>
